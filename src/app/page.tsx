4109a8df5040af0ad54f53352f5a087222474cbb
guhrodriguess <gumartinsR1201@gmail.com>
2023-09-21 14:07:39 -0300

    feat: Updating code semantic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Chat } from "@/components/Chat";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center px-2">
+    <main className="flex min-h-screen items-center justify-center px-2">
       <Chat />
-    </div>
+    </main>
   );
 }
