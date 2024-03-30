b5d041cb8da82c31965b5ce21f242ffd464d9f51
guhrodriguess <gumartinsR1201@gmail.com>
2024-03-30 00:04:16 -0300

    feat: add new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,33 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'QuickChat',
+  title: "QuickChat",
   description:
-    'The chatbot developed with VercelSDK is the perfect solution for improving customer service and simplifying processes, 24 hours a day, 7 days a week.',
-  creator: 'Gustavo Rodrigues',
+    "The chatbot developed with VercelSDK is the perfect solution for improving customer service and simplifying processes, 24 hours a day, 7 days a week.",
+  creator: "Gustavo Rodrigues",
   authors: [
     {
-      name: 'Gustavo Rodrigues',
-      url: 'https://guhrodrigues.com',
+      name: "Gustavo Rodrigues",
+      url: "https://guhrodrigues.com",
     },
   ],
   icons: {
-    apple: '/favicon.svg',
-    icon: '/favicon.svg',
-    shortcut: '/favicon.svg',
+    icon: ["/favicon.svg"],
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
